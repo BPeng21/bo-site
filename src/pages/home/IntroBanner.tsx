72ab284878bdc9a7f312e17ffb5d0fc672a3fbba
Bo Peng <bo.peng0708@gmail.com>
2025-12-29 13:11:53 +0800

    setup the home page and create the intro banner

---
+++ src/pages/home/IntroBanner.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import { wait } from '../../utilities/time.ts';
+import './IntroBanner.css'
+
+function IntroBanner() {
+  const [isPhaseOne, setIsPhaseOne] = useState(true);
+  const [isHelloVisible, setHelloVisible] = useState(true);
+  const [isPhaseTwo, setIsPhaseTwo] = useState(false);
+  const [isStartVisible, setIsStartVisible] = useState(false);
+  const [isNameVisible, setIsNameVisible] = useState(false);
+
+  useEffect(() => {
+    const introSequence = async () => {
+      await wait(2000);
+      setHelloVisible(false);
+      await wait(500);
+      setIsPhaseOne(false);
+      setIsPhaseTwo(true);
+      await wait(100);
+      setIsStartVisible(true);
+      await wait(2000);
+      setIsNameVisible(true);
+    };
+
+    introSequence();
+  }, []);
+  return (
+    <h1 className="intro-title">
+      {isPhaseOne && (
+        <span className={isHelloVisible ? 'intro-hello' : 'intro-hello hidden'}>
+          Hello
+        </span>
+      )}
+      {isPhaseTwo && (
+        <span className={isStartVisible ? 'intro-start appear' : 'intro-start'}>
+          My name is
+        </span>
+      )}
+      {isPhaseTwo && (
+        <span className={isNameVisible ? 'intro-name appear' : 'intro-name'}>
+          Bo Peng
+        </span>
+      )}
+    </h1>
+  );
+}
+
+export default IntroBanner;
