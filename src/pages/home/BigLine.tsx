df402a260758eb29ac9a9f65ff04208815b8e6b7
Bo Peng <bo.peng0708@gmail.com>
2026-01-04 18:12:54 +0800

    create big line for design

---
+++ src/pages/home/BigLine.tsx
@@ -0,0 +1,22 @@
+import './BigLine.css';
+
+function BigLine() {
+  return (
+    <svg
+      className="big-drawn-line"
+      width="4"
+      height="801"
+      viewBox="0 0 4 801"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.03545 0.00100708L1.5 800.501"
+        stroke="black"
+        strokeWidth="3"
+      />
+    </svg>
+  );
+}
+
+export default BigLine;
