5543a2158326fab22390728366bb6e60c1a2ba66
Bo Peng <bo.peng0708@gmail.com>
2026-01-13 17:47:48 +0800

    change big center line to add a bottom end line

--- src/pages/home/BigLine.tsx
+++ src/pages/home/BigLine.tsx
@@ -1,20 +1,26 @@
 import './BigLine.css';
 
 function BigLine() {
-
   return (
     <svg
       className="big-drawn-line"
-      width="4"
-      height="801"
-      viewBox="0 0 4 801"
+      width="124"
+      height="828"
+      viewBox="0 0 124 828"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M2.03545 0.00100708L1.5 800.501"
+        d="M62.4993 0.00100708L61.9475 825.001"
         stroke="black"
-        strokeWidth="3"
+        stroke-width="3"
+      />
+      <line
+        y1="826.001"
+        x2="124"
+        y2="826.001"
+        stroke="black"
+        stroke-width="3"
       />
     </svg>
   );
