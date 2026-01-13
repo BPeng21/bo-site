95c58c3bebe2f5ca6b0000d27ab535432949041a
Bo Peng <bo.peng0708@gmail.com>
2026-01-13 22:32:03 +0800

    start creating me and my career section

---
+++ src/pages/home/MeAndCareer.tsx
@@ -0,0 +1,11 @@
+import './MeAndCareer.css';
+
+function MeAndCareer() {
+  return (
+    <section>
+      <img className="my-photo" src="images/my-photo.jpg" />
+    </section>
+  );
+}
+
+export default MeAndCareer;
