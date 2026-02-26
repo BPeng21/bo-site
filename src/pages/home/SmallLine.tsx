cef477afadbce86e1793f839b38e31ebd5dafb2e
Bo Peng <bo.peng0708@gmail.com>
2026-02-26 16:36:52 +0800

    create first interactive button for tech info

---
+++ src/pages/home/SmallLine.tsx
@@ -0,0 +1,25 @@
+import './SmallLine.css';
+
+function SmallLine() {
+  return (
+    <svg
+      className="small-drawn-line"
+      width="401"
+      height="123"
+      viewBox="0 0 401 123"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line
+        x1="0.431022"
+        y1="1.43674"
+        x2="400.431"
+        y2="121.437"
+        stroke="black"
+        strokeWidth="3"
+      />
+    </svg>
+  );
+}
+
+export default SmallLine;
--- src/pages/home/TechSummary.tsx
+++ src/pages/home/TechSummary.tsx
@@ -1,25 +1,47 @@
+import { useState } from 'react';
+import SmallLine from './SmallLine';
+import SnazzyButton from './SnazzyButton';
 import './TechSummary.css';
 
 function TechSummary() {
+  const [isCardOpen, setIsCardOpen] = useState(false);
+
   return (
     <section className="tech-summary">
-      <article className="card-right">
-        <h2>Full-Stack Developer</h2>
-        <p>
-          A Software developer with 3 years of professional experience working
-          with both the front-end and back-end technology, I helped build large-scale projects in a highly dynamic environment where my adaptability skills were put to the test through frequent context switching between projects as well as technologies.
-        </p>
-      </article>
+      <div className="info-container">
+        <div aria-hidden="true" />
+        <div className="node-right">
+          <SmallLine />
+          <SnazzyButton setIsCardOpen={setIsCardOpen} />
+          {isCardOpen && (
+            <article className="card-right">
+              <h2>Full-Stack Developer</h2>
+              <p>
+                A Software developer with 3 years of professional experience
+                working with both the front-end and back-end technology, I
+                helped build large-scale projects in a highly dynamic
+                environment where my adaptability skills were put to the test
+                through frequent context switching between projects as well as
+                technologies.
+              </p>
+            </article>
+          )}
+        </div>
+      </div>
       <article className="card-left">
         <h2>React-focused Web Dev</h2>
         <p>
-          My primary focus has been react-based web development. I have a strong foundation in JavaScript while slowly honing my skills in TypeScript with React. In fact, this website is built on Typescript and React!
+          My primary focus has been react-based web development. I have a strong
+          foundation in JavaScript while slowly honing my skills in TypeScript
+          with React. In fact, this website is built on Typescript and React!
         </p>
       </article>
       <article className="card-right">
         <h2>Immersive Web Experience</h2>
         <p>
-          I strive to develop web applications that give users a great experience. I want my website to feel alive, but also keep it easy on the eye. Hope you enjoy!
+          I strive to develop web applications that give users a great
+          experience. I want my website to feel alive, but also keep it easy on
+          the eye. Hope you enjoy!
         </p>
       </article>
     </section>
