b20113288ce4b10f8a9a511f99d3a47a099d5a9d
Bo Peng <bo.peng0708@gmail.com>
2026-02-26 16:36:52 +0800

    create arrow indicator for navBar

---
+++ src/components/NavArrow.tsx
@@ -0,0 +1,20 @@
+function NavArrow({ ...props }) {
+  return (
+    <svg
+      className={props.className}
+      width="100%"
+      height="100%"
+      viewBox="0 0 351 271"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="none"
+    >
+      <path
+        d="M175.5 0C175.5 149.669 254.074 271 351 271H0C96.926 271 175.5 149.669 175.5 0Z"
+        fill="rgb(242, 238, 226)"
+      />
+    </svg>
+  );
+}
+
+export default NavArrow;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,29 @@
 import { Link } from 'react-router';
-import './NavBar.css'
+import NavArrow from './NavArrow';
+import { useLocation } from 'react-router';
+import './NavBar.css';
 
 function NavBar() {
+  const { pathname } = useLocation();
+
   return (
     <nav className="site-nav">
       <ul className="page-links">
         <Link to="/" className="home-link">
           <li>Home</li>
+          <NavArrow className={pathname == '/' ? "nav-arrow current" : "nav-arrow"}/>
         </Link>
         <Link to="about" className="about-link">
           <li>About</li>
+          <NavArrow className={pathname == '/about' ? "nav-arrow current" : "nav-arrow"}/>
         </Link>
-        <Link to="blog" className='blog-link'>
+        <Link to="blog" className="blog-link">
           <li>Blog</li>
+          <NavArrow className={pathname == '/blog' ? "nav-arrow current" : "nav-arrow"}/>
         </Link>
         <Link to="resume" className="resume-link">
           <li>Resume</li>
+          <NavArrow className={pathname == '/resume' ? "nav-arrow current" : "nav-arrow"}/>
         </Link>
       </ul>
     </nav>
