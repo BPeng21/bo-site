93f5be23d7c8eded497fce55c9c2b9638d97d5ea
Bo Peng <bo.peng0708@gmail.com>
2026-04-09 18:30:00 +0800

    start about page and create functioning wheel of my hobbies and section of hobby info

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+import HobbyWheel from './Icons/HobbyWheel';
+import HobbyContent from './HobbyContent/HobbyContent';
+import './AboutPage.css'
+
 function AboutPage() {
-  return <div></div>;
+  const [hobbyIndex, setHobbyIndex] = useState(0);
+
+  return (
+    <div>
+      <h1 className="sr-only">About Web Developer and Hobbies</h1>
+      <HobbyWheel setHobbyIndex={setHobbyIndex} />
+      <HobbyContent hobbyIndex={hobbyIndex} />
+    </div>
+  );
 }
 
 export default AboutPage;
---
+++ src/pages/about/Icons/HobbyWheel.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState, useRef } from 'react';
+import Wheel from './../../../assets/wheel.svg';
+import './HobbyWheel.css';
+
+interface Props {
+  setHobbyIndex: (hobbyIndex: number) => void;
+}
+
+const SPIN_DURATION = 6;
+
+function HobbyWheel({ setHobbyIndex }: Props) {
+  const [rotation, setRotation] = useState(0);
+  const isDragging = useRef(false);
+  const startY = useRef(0);
+  const startRotation = useRef(0);
+  const controlledSpinRotation = useRef(0);
+
+  function handleMouseDown(event: React.MouseEvent) {
+    isDragging.current = true;
+    startY.current = event.clientY;
+    startRotation.current = rotation % 360;
+    document.body.style.cursor = 'grabbing';
+    setRotation(startRotation.current);
+  }
+
+  function handleMouseMove(event: MouseEvent) {
+    if (!isDragging.current) return;
+
+    const amountMovedY = event.clientY - startY.current;
+
+    setRotation(startRotation.current + amountMovedY / 2);
+  }
+
+  function handleMouseUp() {
+    isDragging.current = false;
+    document.body.style.cursor = 'default';
+
+    let calculatedFinalRotation = 0;
+
+    if (rotation - startRotation.current < 30) {
+      setRotation(startRotation.current);
+    } else {
+      controlledSpinRotation.current += 15;
+      controlledSpinRotation.current %= 75;
+
+      calculatedFinalRotation = Math.floor((rotation - startRotation.current) / 30) * 360 +
+      controlledSpinRotation.current;
+ 
+      setRotation(calculatedFinalRotation);
+    }
+
+    setHobbyIndex((calculatedFinalRotation % 360) / 15);
+  }
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [rotation]);
+
+  return (
+    <div className="wheel-wrapper">
+      <img
+        src={Wheel}
+        alt="wheel about me"
+        className="wheel-image"
+        draggable="false"
+        onMouseDown={handleMouseDown}
+        style={{
+          transform: `rotate(${rotation}deg)`,
+          transition: isDragging.current
+            ? 'none'
+            : `transform ${SPIN_DURATION}s cubic-bezier(0.1, 0.9, 0.2, 1)`,
+        }}
+      ></img>
+    </div>
+  );
+}
+
+export default HobbyWheel;
