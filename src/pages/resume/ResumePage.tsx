5116bbeccf274173aff2d76c861ba4e2cc3a687f
Bo Peng <bo.peng0708@gmail.com>
2026-06-30 11:57:27 +0800

    create work-in-progress resume page

--- src/pages/resume/ResumePage.tsx
+++ src/pages/resume/ResumePage.tsx
@@ -1,5 +1,12 @@
+import './ResumePage.css'
+
 function ResumePage() {
-  return <div></div>;
+  return (
+    <main className="site-resume">
+      <h1 className="status-notice">Coming Soon</h1>
+      <p>(in the meantime...here is the <a href="/my-resume.pdf" target="_blank">pdf version</a>)</p>
+    </main>
+  );
 }
 
 export default ResumePage;
